819640562407153ff016dbb0c850663739aee5a8
renanfigueoli09 <renanfigueoli@outlook.com>
2025-06-18 23:07:19 -0300

    feat: 🎸 adding getUser widh webSocket

---
+++ src/modules/user/gateway/user.gateway.ts
@@ -0,0 +1,125 @@
+import { WebSocketService } from "@common/services/webSocket.service";
+import { configService } from "@config/configService";
+import { JwtService } from "@nestjs/jwt";
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from "@nestjs/websockets";
+import { UserPagination } from "@user/models/userPagination.model";
+import { UsersDocument } from "@user/schemas/user.schema";
+import { UserService } from "@user/services/user.service";
+import { Server, WebSocket } from "ws";
+
+@WebSocketGateway({
+  path: "/user",
+  transports: ["websocket"],
+  cors: { origin: "*" },
+})
+export class UserGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  constructor(
+    private readonly webSocketService: WebSocketService,
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+  async handleConnection(client: WebSocket, req: Request) {
+    const baseUrl = configService.get<string>("URL") || "http://localhost:3000";
+    const url = new URL(req.url, baseUrl);
+    const userId = url.searchParams.get("userId") as string;
+    if (!userId) {
+      return client.close(1008, "param userId not found");
+    }
+    const authHeader = req.headers["authorization"];
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return client.close(1008, "Missing or invalid authorization header");
+    }
+    const token = authHeader.split(" ")[1];
+    try {
+      const payload = this.jwtService.verify(token);
+      if (!payload) {
+        return client.close(1008, "Unauthorized");
+      }
+    } catch {
+      return client.close(1008, "Unauthorized");
+    }
+    try {
+      await this.userService.getUserByID(userId);
+    } catch {
+      return client.close(1008, "user not found");
+    }
+
+    this.webSocketService.addClient(userId, client);
+    this.webSocketService.setServer(this.server);
+
+    this.webSocketService.broadcast("user.online", {
+      userId,
+      status: "online",
+    });
+
+    client.on("message", (message) => {
+      this.webSocketService.handleMessage(client, message.toString());
+    });
+  }
+
+  handleDisconnect(client: WebSocket) {
+    const userId = [...this.webSocketService.usersOnline.entries()].find(
+      ([, socket]) => socket === client,
+    )?.[0];
+
+    if (userId) {
+      this.webSocketService.removeClient(userId);
+
+      this.webSocketService.broadcast("user.offline", {
+        userId,
+        status: "offline",
+      });
+    }
+  }
+  @SubscribeMessage("user")
+  async getUser(
+    @MessageBody() body: { page?: number; perPage?: number },
+  ): Promise<{
+    event: string;
+    data: UserPagination | Error;
+  }> {
+    try {
+      const result: UserPagination = await this.userService.getUsers(
+        body?.page ? parseInt(body?.page.toString()) : 1,
+        body?.perPage ? parseInt(body?.perPage.toString()) : 10,
+      );
+      return {
+        event: "user",
+        data: result,
+      };
+    } catch (error) {
+      return {
+        event: "error",
+        data: error?.response || error,
+      };
+    }
+  }
+  @SubscribeMessage("user.id")
+  async getUserById(@MessageBody() body: { id: string }): Promise<{
+    event: string;
+    data: UsersDocument | Error;
+  }> {
+    try {
+      const result: UsersDocument = await this.userService.getUserByID(body.id);
+      return {
+        event: "user.id",
+        data: result,
+      };
+    } catch (error) {
+      return {
+        event: "error",
+        data: error?.response || error,
+      };
+    }
+  }
+}
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,15 +4,18 @@ import { UserService } from "./services/user.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Users } from "./models/user.model";
 import { Users_schema } from "./schemas/user.schema";
+import { UserGateway } from "./gateway/user.gateway";
+import { CommonModule } from "@common/common.module";
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Users.name, schema: Users_schema, collection: Users.name },
     ]),
+    CommonModule,
   ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserGateway],
   exports: [UserService],
 })
 export class UserModule {}
