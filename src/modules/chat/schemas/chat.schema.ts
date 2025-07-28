022e811f1fc2adf3949f03b316dfe472577255be
renanfigueoli09 <renanfigueoli@outlook.com>
2025-07-28 20:12:34 -0300

    feat: 🎸 addin new fields

--- src/modules/chat/schemas/chat.schema.ts
+++ src/modules/chat/schemas/chat.schema.ts
@@ -1,6 +1,18 @@
 import { SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument } from "mongoose";
+import {
+  CallbackWithoutResultAndOptionalError,
+  HydratedDocument,
+} from "mongoose";
 import { Chats } from "../models/chat.model";
+import { DateService } from "@common/services/date.service";
 
 export const Chats_schema = SchemaFactory.createForClass(Chats);
 export type ChatsDocument = HydratedDocument<Chats>;
+Chats_schema.pre(
+  "save",
+  function (next: CallbackWithoutResultAndOptionalError) {
+    this.createdAt ??= new DateService().now();
+    this.updatedAt ??= new DateService().now();
+    next();
+  },
+);
